a8152f3e784ce37d86bc9e6ede4236b9e465a04a
Benjamin Schreck <benjaminschreck93@gmail.com>
2018-03-23 08:40:10 -0700

    fl

--- dask_dfs.py
+++ dask_dfs.py
@@ -20,74 +20,76 @@ ft.__version__
 
 
 # In[ ]:
-
-
-pbar = ProgressBar()
-pbar.register()
-
-
-# In[ ]:
-
-
-path = "partitioned_data/"
-_, dirnames, _ = next(os.walk(path))
-dirnames = [path+d for d in dirnames]
-b = bag.from_sequence(dirnames)
-entity_sets = b.map(utils.load_entityset)
-
-
-# In[ ]:
-
-
-cutoff_time = pd.Timestamp('March 1, 2015')
-training_window = ft.Timedelta("60 days")
-
-
-# In[ ]:
-
-
-label_times = entity_sets.map(utils.make_labels,
-                              product_name="Banana",
-                              cutoff_time=cutoff_time,
-                              prediction_window=ft.Timedelta("4 weeks"),
-                              training_window=training_window)
+#
+#
+#pbar = ProgressBar()
+#pbar.register()
+#
+#
+## In[ ]:
+#
+#
+#path = "partitioned_data/"
+#_, dirnames, _ = next(os.walk(path))
+#dirnames = [path+d for d in dirnames]
+#b = bag.from_sequence(dirnames)
+#entity_sets = b.map(utils.load_entityset)
+#
+#
+## In[ ]:
+#
+#
+#cutoff_time = pd.Timestamp('March 1, 2015')
+#training_window = ft.Timedelta("60 days")
+#
+#
+## In[ ]:
+#
+#
+#label_times = entity_sets.map(utils.make_labels,
+#                              product_name="Banana",
+#                              cutoff_time=cutoff_time,
+#                              prediction_window=ft.Timedelta("4 weeks"),
+#                              training_window=training_window)
 
 
 # In[ ]:
 
 
 trans_primitives = [Day, Weekend, Weekday, Percentile]
-def dask_tdfs(entityset, **kwargs):
-    return tdfs(entityset=entityset, **kwargs)
-tdfs_results = entity_sets.map(dask_tdfs,
-                               target_entity="users",
-                               cutoffs=label_times,
-                               trans_primitives=trans_primitives,
-                               training_window=training_window,
-                               window_size='3d',
-                               start=cutoff_time - training_window,
-                               verbose=True)
+#def dask_tdfs(entityset, **kwargs):
+#    return tdfs(entityset=entityset, **kwargs)
+#tdfs_results = entity_sets.map(dask_tdfs,
+#                               target_entity="users",
+#                               cutoffs=label_times,
+#                               trans_primitives=trans_primitives,
+#                               training_window=training_window,
+#                               window_size='3d',
+#                               start=cutoff_time - training_window,
+#                               verbose=True)
+#
+#
+## In[ ]:
+#
+#
+#label_times, tdfs_results = bag.compute(label_times, tdfs_results)
+#
+#labels = pd.concat(label_times).set_index('user_id').sort_index()['label']
+#fm = pd.concat([r[0] for r in tdfs_results]).sort_index()
+#fl = tdfs_results[0][1]
 
 
 # In[ ]:
 
 
-label_times, tdfs_results = bag.compute(label_times, tdfs_results)
-
-labels = pd.concat(label_times).set_index('user_id').sort_index()['label']
-fm = pd.concat([r[0] for r in tdfs_results]).sort_index()
-fl = tdfs_results[0][1]
-
-
-# In[ ]:
-
-
-fm.to_csv("fm_full_data.csv")
-labels.to_frame().to_csv("label_times_full_data.csv")
+#fm.to_csv("fm_full_data.csv")
+#labels.to_frame().to_csv("label_times_full_data.csv")
+es = utils.load_entityset('partitioned_data/part_0/')
+fl = ft.dfs(entityset=es, target_entity="users", trans_primitives=trans_primitives, features_only=True)
+ft.save_features(fl, 'fl.p')
 fm = pd.read_csv("fm_full_data.csv", index_col=["user_id", "time"], parse_dates=["time"])
 fm.reset_index('time', drop=True, inplace=True)
 labels = pd.read_csv("label_times_full_data.csv", index_col=["user_id"])["label"]
-es = utils.load_entityset('partitioned_data/part_0/')
 fl = ft.load_features('fl.p', es)
 
 print("loaded")
