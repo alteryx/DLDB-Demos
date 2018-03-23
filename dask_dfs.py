bb47d9c694e268bc22715b97016e3fea93b5c795
Benjamin Schreck <benjaminschreck93@gmail.com>
2018-03-22 21:27:25 -0700

    additional cutoffs

--- dask_dfs.py
+++ dask_dfs.py
@@ -14,78 +14,82 @@ from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import StratifiedKFold
 from dldb import DLDB, tdfs, make_temporal_cutoffs
 from keras.callbacks import EarlyStopping
-import os
+from featuretools.primitives import Day, Weekend, Weekday, Percentile
+import os, sys
 ft.__version__
 
 
 # In[ ]:
 
 
-# pbar = ProgressBar()
-# pbar.register()
+pbar = ProgressBar()
+pbar.register()
 
 
-# # In[ ]:
+# In[ ]:
 
 
-# path = "partitioned_data/"
-# _, dirnames, _ = next(os.walk(path))
-# dirnames = [path+d for d in dirnames]
-# b = bag.from_sequence(dirnames)
-# entity_sets = b.map(utils.load_entityset)
+path = "partitioned_data/"
+_, dirnames, _ = next(os.walk(path))
+dirnames = [path+d for d in dirnames]
+b = bag.from_sequence(dirnames)
+entity_sets = b.map(utils.load_entityset)
 
 
-# # In[ ]:
+# In[ ]:
 
 
-# cutoff_time = pd.Timestamp('March 1, 2015')
-# training_window = ft.Timedelta("60 days")
+cutoff_time = pd.Timestamp('March 1, 2015')
+training_window = ft.Timedelta("60 days")
 
 
-# # In[ ]:
+# In[ ]:
 
 
-# label_times = entity_sets.map(utils.make_labels,
-                              # product_name="Banana",
-                              # cutoff_time=cutoff_time,
-                              # prediction_window=ft.Timedelta("4 weeks"),
-                              # training_window=training_window)
+label_times = entity_sets.map(utils.make_labels,
+                              product_name="Banana",
+                              cutoff_time=cutoff_time,
+                              prediction_window=ft.Timedelta("4 weeks"),
+                              training_window=training_window)
 
 
-# # In[ ]:
+# In[ ]:
 
 
-# tdfs_results = entity_sets.map(tdfs,
-                               # target_entity="users",
-                               # cutoffs=label_times,
-                               # training_window=training_window,
-                               # window_size='14d',
-                               # num_windows=5,
-                               # verbose=True)
+trans_primitives = [Day, Weekend, Weekday, Percentile]
+tdfs_results = entity_sets.map(tdfs,
+                               target_entity="users",
+                               cutoffs=label_times,
+                               trans_primitives=trans_primitives,
+                               training_window=training_window,
+                               window_size='3d',
+                               start=cutoff_time - training_window,
+                               verbose=True)
 
 
-# # In[ ]:
+# In[ ]:
 
 
-# label_times, tdfs_results = bag.compute(label_times, tdfs_results)
+label_times, tdfs_results = bag.compute(label_times, tdfs_results)
 
-# labels = pd.concat(label_times).set_index('user_id').sort_index()['label']
-# fm = pd.concat([r[0] for r in tdfs_results]).sort_index()
-# fl = tdfs_results[0][1]
-
-
-# # In[ ]:
+labels = pd.concat(label_times).set_index('user_id').sort_index()['label']
+fm = pd.concat([r[0] for r in tdfs_results]).sort_index()
+fl = tdfs_results[0][1]
 
 
-#fm.to_csv("fm_full_data.csv")
-#labels.to_frame().to_csv("label_times_full_data.csv")
+# In[ ]:
+
+
+fm.to_csv("fm_full_data.csv")
+labels.to_frame().to_csv("label_times_full_data.csv")
 fm = pd.read_csv("fm_full_data.csv", index_col=["user_id", "time"], parse_dates=["time"])
-fm.reset_index('order_time', drop=True, inplace=True)
+fm.reset_index('time', drop=True, inplace=True)
 labels = pd.read_csv("label_times_full_data.csv", index_col=["user_id"])["label"]
 es = utils.load_entityset('partitioned_data/part_0/')
 fl = ft.load_features('fl.p', es)
 
 print("loaded")
+sys.stdout.flush()
 # In[ ]:
 
 dl_model = DLDB(
@@ -100,6 +104,7 @@ dl_model = DLDB(
 # TODO: cheating a bit, put back in CV later
 dl_model.compile(fm, fl=fl)
 print("compiled")
+sys.stdout.flush()
 
 
 # In[ ]:
@@ -119,7 +124,7 @@ for i, train_test_index in enumerate(splitter.split(labels, labels)):
     dl_model.fit(
         train_fm, train_labels,
         validation_split=0.1,
-        epochs=10,
+        epochs=1,
         batch_size=256)
 
     predictions = dl_model.predict(test_fm)
